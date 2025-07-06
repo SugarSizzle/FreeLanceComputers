4fcb88397585ee67583f285b6ede59521e3605af
SugarSizzle <george.leon41@yahoo.com>
2025-07-06 11:48:19 -0400

    Services styling, added Footer, and Virus protection page, organized files
    I made a lot of stuff here, I added a virus protection page, started react router, fixed some issues with the home page services section, added a footer.

---
+++ src/Pages/VirusProtection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigation } from '../Layout/Navigation'
+import { Footer } from '../Layout/Footer'
+import { VirusProtectoinHeroSection } from '../components/VirusProtectoinHeroSection'
+export const VirusProtection = () => {
+    return (
+        <>
+            <Navigation/>
+            <VirusProtectoinHeroSection/>
+
+
+            <Footer/>
+        </>
+    )
+}
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,15 +1,15 @@
 import React, {useState} from 'react'
-import ComputerRepairs from '../images/ComputerRepairs.jpg'
-import ComputerUpgrades from '../images/ComputerUpgrades.jpg'
-import DataRecovery from '../images/DataRecovery.jpg'
-import VirusRemoval from '../images/Virus.jpg'
-
+import ComputerRepairs from '../../images/ComputerRepairs.jpg'
+import ComputerUpgrades from '../../images/ComputerUpgrades.jpg'
+import DataRecovery from '../../images/DataRecovery.jpg'
+import VirusRemoval from '../../images/Virus.jpg'
+import '../../App.css'
 
 const servicesText =[
-    'We Remove Viruses, Malware, and other threats from your computer.',
-    'We Recover Data from your computer.',
-    'We Upgrade your computer to the latest and greatest.',
-    'We Repair your computer.'
+    'Virus Removal',
+    'Data Recovery',
+    'Computer Upgrades',
+    'Computer Repairs'
 ]
 
 const servicesImage =[
@@ -19,48 +19,69 @@ const servicesImage =[
     VirusRemoval
 ]
 
-// const [serviceTextState, setServiceTextState] = useState(servicesText)
-// const [serviceImageState, setServiceImageState] = useState()
 
 export const Services = () => {
+  const [selectedService, setSelectedService] = useState('Virus Removal')
+  const [theme, setTheme] = useState('.selected-service')
+
+
+  const mappedServices=servicesText.map((service, index)=>{
+    return(
+      <div  className={`individual-service-container ${theme}`} key={index} onClick={()=>setSelectedService(service)}>
+        <h3 className='service-title'>{service}</h3>
+      </div>
+    )
+  })
+
+  
+
+  
+  
+
+  function BackgroundColorServiceText(){
+    if(selectedService === 'Virus Removal'){
+      
+    }
+    else if(selectedService === 'Data Recovery'){
+      return 'blue'
+    }
+    else if(selectedService === 'Computer Upgrades'){
+      return 'green'
+    }
+    else if(selectedService === 'Computer Repairs'){
+      return 'yellow'
+    }
+  }
+
+
+
+
+
+
+
+
+
 
   return (
-<>
-<h1 className="services-header-title">Services</h1>
-    <div className='services-container'>
-      <div className="services-section-container">
-
-        <div className="individual-service-container">
-            <h3 className='service-title'>Virus Removal</h3>
+    <>
+      <h1 className="services-header-title">Services</h1>
+      <div className='services-container'>
+        <div className="services-section-container">
+          {mappedServices}
         </div>
 
-        <div className="individual-service-container">
-            <h3 className='service-title'>Data Recovery</h3>
-        </div>
+        <div className='services-information-container'>
+          <img src={ComputerRepairs} className='services-information-image' alt="image of service here" />
+          <p className='services-information-text'>
+            We Remove Viruses, Malware, and other threats from your computer.
+            Here are some of the things the right way, and make sure you get the best service.
+          </p>
 
-        <div className="individual-service-container">
-            <h3 className='service-title'>Computer Upgrades</h3>
-        </div>
-
-        <div className="individual-service-container">
-            <h3 className='service-title'>Computer Repairs</h3>
+          <div className="services-get-touch-container">
+            <button className="services-get-touch-button">Get In Touch</button>
+          </div>
         </div>
       </div>
-
-    <div className='services-information-container'>
-        <img src={ComputerRepairs} className='services-information-image' alt="image of service here" />
-        <p className='services-information-text'>
-        We Remove Viruses, Malware, and other threats from your computer.
-        Here are some of the things the right way, and make sure you get the best service.
-        </p>
-
-        <div className="services-get-touch-container">
-          <button className="services-get-touch-button">Get In Touch</button>
-        </div>
-    </div>
-
-
-    </div>
-</>
+    </>
   )
 }
