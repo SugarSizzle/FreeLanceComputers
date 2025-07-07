9a92823ff2cc74465c8c36966e5690aa7abe9325
SugarSizzle <george.leon41@yahoo.com>
2025-07-07 05:43:57 -0400

    Finished Virus Protection Page
    Finished off the Virus Protection Page, added some styling and basic layout for mobile version.

---
+++ src/components/VirusProtectionInformation.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+        
+
+export const VirusProtectionInformation = () => {
+    return (
+    <>
+        
+        <div className='virus-protection-information-container'>
+        <h3 className="virus-protection-information-header">Security</h3>
+            <h2 className="virus-protection-information-title">Secure Yourself From Security Threats</h2>
+            <p className="virus-protection-information-text">In Today’s Age, You Can Never Be
+                Too Cautious. Make Sure You Protect 
+                Yourself Before Your Private Info 
+                Gets Stolen
+            </p>
+        </div>        
+
+        <div className="virus-protection-images-container">
+            <div className="virus-security-code-image" />
+            <div className='virus-security-image'/>
+            
+            
+        </div>
+
+
+           
+         
+        
+    </>
+    )
+}
