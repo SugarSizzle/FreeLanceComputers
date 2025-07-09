50cf6ff49f71c2bb13bb2b5311798077dba3fc72
SugarSizzle <george.leon41@yahoo.com>
2025-07-09 15:12:21 -0400

    Finished Computer Repairs
    I finished the Computer Repairs page for the website, next up will be to finish setting up the shop section/pages. Then we can start polishing our website before making it responsive.

---
+++ src/components/ComputerReapairsHeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export const ComputerRepairsHeroSection = () => {
+
+    return(
+    <>
+        <div className='computer-repairs-hero-3D-section-container'>
+            <h3>3D Object Here</h3>
+        </div>
+
+        <div className='computer-repairs-hero-section-header-container'>
+            <p className='computer-repairs-hero-section-header'><span className='computer-repairs-hero-section-header-span'>Revive</span> Your 
+            <br />Computer </p>
+        </div>
+    </>
+    )
+
+
+}
--- src/components/VirusGetInTouch.jsx
+++ src/components/VirusGetInTouch.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export const GetInTouch = () => {
+export const VirusGetInTouch = () => {
     return (
         <>
             <div className='get-in-touch-container'>
