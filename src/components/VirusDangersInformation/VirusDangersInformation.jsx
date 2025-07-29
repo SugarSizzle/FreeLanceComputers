88d113948b2712fb877d8bac937b17b679b2620f
SugarSizzle <george.leon41@yahoo.com>
2025-07-29 14:17:44 -0400

    Finished styling VirusForm
    I added hover effects, animations, and a lot of styling to the virus page form

--- src/components/VirusDangersInformation/VirusDangersInformation.jsx
+++ src/components/VirusDangersInformation/VirusDangersInformation.jsx
@@ -4,45 +4,28 @@ import {motion, useScroll, useTransform} from 'framer-motion'
 
 export const VirusDangersInformation = () => {
 
-    const red = useRef(null)
+
+    const ref = useRef(null)
    
 
     const {scrollYProgress} = useScroll({
-        target: red,
+        target: ref,
         offset: ["start 100%", "end 1%"]
     })
-    const scaleText = useTransform(scrollYProgress, [0.00, .4], [.8, 1])
-    const opacityText = useTransform(scrollYProgress, [0.00, .4], [0, 1])
+    const scaleText = useTransform(scrollYProgress, [0.00, .3], [.8, 1])
+    const opacityText = useTransform(scrollYProgress, [0.00, .3], [0, 1])
     const scaleImage = useTransform(scrollYProgress, [0.43, .61], [.6, 1])
     const opacityImage = useTransform(scrollYProgress, [0.5, .75], [0, 1])
 
    
+    
 
 
-    // useEffect(() => {   
-    //     const unsubscribe = scrollYProgressTextContainer.on("change", (value) => {
-    //         console.log(` Scroll Y Progress Text ` , value.toFixed(2))
-    //     })
-
-    //     return () => unsubscribe()
-
-    // }, [scrollYProgressTextContainer])
-
-
-    useEffect(() => {
-
-            const unsubscribe = scrollYProgress.on("change", (value) => {
-                console.log(` Scroll Y Progress` , value.toFixed(2))
-            })
-
-            return () => unsubscribe()
-
-    }, [scrollYProgress])
 
 
     return (
         <>
-        <motion.div className={styles.virusDangersContainer} ref={red} >
+        <motion.div className={styles.virusDangersContainer} ref={ref} >
             <motion.div  style={{scale: scaleText, opacity: opacityText}} className={styles.virusDangersInformationContainer}>
                 <h3 className={styles.virusDangersInformationHeader}>Defects</h3>
                 <h2 className={styles.virusDangersInformationTitle}>A Virus Can Slow Down The Performance Of Your Machine</h2>
