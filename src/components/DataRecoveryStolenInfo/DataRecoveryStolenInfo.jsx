08e27f4d3be5b602750f58886c9b3539ef6fce64
SugarSizzle <george.leon41@yahoo.com>
2025-08-05 14:35:43 -0400

    Added A lot of styling
    Beginning of page transitions, added scroll effects to some pages, fixed bugs.

--- src/components/DataRecoveryStolenInfo/DataRecoveryStolenInfo.jsx
+++ src/components/DataRecoveryStolenInfo/DataRecoveryStolenInfo.jsx
@@ -1,28 +1,71 @@
-import React from 'react'
+import React, { useRef , useEffect } from 'react'
 import styles from './DataRecoveryStolenInfo.module.css'
-
+import { motion , useScroll, useTransform } from 'framer-motion'
 
 
 
 export const DataRecoveryStolenInfo = () => {
 
+    const TextRef = useRef(null)
+    const imageRef = useRef(null)
+
+
+
+    const {scrollYProgress: textScrollYProgress} = useScroll({
+        target: TextRef,
+        offset: ['start 90%' , 'end 20%']
+    })
+
+
+    const {scrollYProgress: imageScrollYProgress} = useScroll({
+        target: imageRef,
+        offset: ['start 100%', 'end 10%']
+    })
+
+    const Textopacity = useTransform(textScrollYProgress, [.14, .55], [0, 1])
+    const textY = useTransform(textScrollYProgress, [.14, .55 ], [200, 0])
+    const textScale = useTransform(textScrollYProgress, [.14, .55 ], [.85, 1])
+
+
+    const imageOpacity = useTransform(imageScrollYProgress, [.2, .44], [0, 1])
+    const imageY = useTransform(imageScrollYProgress, [.14, .44], [200, 0])
+    const imageScale = useTransform(imageScrollYProgress, [.14, .44], [.85, 1])
+   
+  
+
+
     return (
         <>
-        <div className={styles.dataRecoveryStolenInfoContainer}>
+        <motion.div 
+        ref={TextRef} 
+        className={styles.dataRecoveryStolenInfoContainer}
+        style={{opacity:Textopacity, y:textY, scale:textScale}}
+        // whileInView={{opacity:1, y:0, scale:1}}
+        // initial={{opacity:0, y:200, scale:.85}}
+        transition={{duration:.3, ease:'easeInOut'}}
+        >
+       
+       
 
             <p className={styles.dataRecoveryStolenInfoRecoveryText}>Recovery</p>
             <h3 className={styles.dataRecoveryStolenInfoHeader}>Stolen Personal Information Happens Every Day</h3>
             <p className={styles.dataRecoveryStolenInfoText}>Losing Your Personal Information Can Change Your Life, and Happens 
                Frequently. Make Sure You Know Someone To Go To When It Happens. </p>
 
-        </div>
+        </motion.div>
 
-        <div className={styles.dataRecoveryStolenInfoImageContainer}>
-            <div className={styles.dataRecoveryStolenInfoImage}/>\
+        <motion.div 
+        ref={imageRef}
+        style={{opacity:imageOpacity, y:imageY, scale:imageScale}} 
+        // whileInView={{opacity:1, y:0, scale:1}}
+        // initial={{opacity:0, y:200, scale:.85}}
+        // transition={{duration:.3, ease:'easeInOut'}}
+        className={styles.dataRecoveryStolenInfoImageContainer}>
+            <div className={styles.dataRecoveryStolenInfoImage}/>
             <div className={styles.dataRecoveryStolenInfoCodeImage}/>
             
 
-        </div>
+        </motion.div>
 
 
         </>
