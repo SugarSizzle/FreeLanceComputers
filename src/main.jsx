6649c4e7de461a54615b7dc5f0a4f92a0286cf30
SugarSizzle <george.leon41@yahoo.com>
2025-08-27 15:01:50 -0400

    Updated to React 19, basic login feature
    I updated to React 19 in this section, and now I am learning how to make log in features.

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { UseVirusContext } from './Context/UseVirusContext'
+import { AuthContextProvider } from './Context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <UseVirusContext>
-        <App />
-      </UseVirusContext>
+      <AuthContextProvider>
+        <UseVirusContext>
+          <App />
+        </UseVirusContext>
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 ) 
