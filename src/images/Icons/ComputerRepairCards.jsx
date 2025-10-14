0eb985d0ce7e184c588ec6ed32a7d2e53f3c50ef
SugarSizzle <george.leon41@yahoo.com>
2025-10-14 14:37:22 -0400

    Added svg Card to overlay
    I added some svgs as cards in hopes that it better performance

---
+++ src/images/Icons/ComputerRepairCards.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import styles from './ComputerRepairCards.module.css';
+
+export const ComputerRepairCards = () => {
+    return (
+
+
+<svg className={styles.computerRepairSvg} width="50vw" height="80%" viewBox="0 0 251 214" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="computer repairs frame">
+    <g id="repairs group">
+    <g id="uil:wrench" clip-path="url(#clip0_370_23)">
+    <g id="Vector" filter="url(#filter0_d_370_23)">
+    <path d="M145.047 82.2738C146.714 85.8496 147.451 89.7884 147.193 93.725C146.934 97.6614 145.688 101.469 143.569 104.796C141.449 108.124 138.524 110.863 135.066 112.761C131.607 114.659 127.726 115.656 123.781 115.657C122.345 115.658 120.913 115.515 119.506 115.229L119.243 115.175L119.052 115.366L104.958 129.491L104.813 129.637V129.747C104.336 130.158 103.728 130.388 103.094 130.388C102.395 130.388 101.724 130.11 101.229 129.616C100.734 129.121 100.457 128.45 100.456 127.75C100.456 127.051 100.734 126.38 101.229 125.885L116.541 110.572C116.862 110.253 117.26 110.023 117.697 109.904C118.132 109.785 118.59 109.78 119.028 109.89C120.578 110.301 122.173 110.516 123.777 110.532H123.782C128.608 110.532 133.237 108.616 136.653 105.206C140.068 101.797 141.992 97.1712 142 92.3451C142.004 91.2706 141.918 90.1973 141.744 89.1371L141.585 88.1811L140.898 88.8637L128.654 101.015H128.653C128.163 101.488 127.509 101.753 126.828 101.753C126.148 101.753 125.493 101.487 125.003 101.014L114.977 91.0824C114.731 90.8384 114.536 90.5478 114.403 90.2279C114.27 89.9082 114.201 89.5655 114.201 89.2191C114.201 88.8726 114.269 88.5292 114.403 88.2094C114.536 87.8895 114.731 87.5989 114.977 87.3549L114.978 87.3539L127.259 75.1039L127.95 74.4154L126.988 74.2562C125.928 74.0814 124.856 73.9964 123.782 74.0004H123.781C121.377 74.0045 118.998 74.484 116.781 75.4115C114.564 76.339 112.552 77.6964 110.862 79.4047C109.171 81.1131 107.835 83.1388 106.931 85.3656C106.027 87.5924 105.573 89.9761 105.594 92.3793C105.609 93.9804 105.825 95.5736 106.235 97.1215L106.234 97.1225C106.345 97.5617 106.341 98.0224 106.221 98.4594C106.101 98.8964 105.87 99.2952 105.551 99.6166H105.552L90.2406 114.928L90.2386 114.929C89.9946 115.175 89.704 115.37 89.3842 115.503C89.0644 115.636 88.7217 115.705 88.3754 115.705C88.0288 115.705 87.6855 115.637 87.3656 115.503C87.0457 115.37 86.7551 115.175 86.5111 114.929L86.5082 114.926C86.2622 114.682 86.0672 114.391 85.934 114.072C85.8008 113.752 85.7319 113.409 85.7318 113.063C85.7318 112.716 85.8007 112.373 85.934 112.053C86.0672 111.733 86.2622 111.443 86.5082 111.199L86.5092 111.198L100.634 97.1039L100.825 96.9145L100.771 96.6508C100.485 95.2438 100.343 93.8111 100.344 92.3754V92.3715C100.319 88.4136 101.296 84.5137 103.183 81.0346C105.07 77.5554 107.807 74.6095 111.138 72.4721C114.469 70.3347 118.288 69.0747 122.237 68.809C126.185 68.5434 130.137 69.2811 133.724 70.9525C134.108 71.1334 134.443 71.4043 134.699 71.7426C134.955 72.0809 135.125 72.4765 135.195 72.8949L135.196 72.9027C135.271 73.3163 135.246 73.7421 135.122 74.1439C135.015 74.4956 134.835 74.8205 134.594 75.0971L134.487 75.2123L120.897 88.7709L120.542 89.1244L126.875 95.4574L127.229 95.1029L140.791 81.5092H140.792L140.907 81.4008C141.181 81.1559 141.505 80.9708 141.857 80.8588C142.259 80.7308 142.686 80.7017 143.102 80.7738H143.103C143.524 80.8463 143.921 81.0209 144.259 81.2816C144.555 81.5098 144.799 81.7983 144.974 82.1273L145.047 82.2738Z" stroke="url(#paint0_linear_370_23)" shape-rendering="crispEdges"/>
+    </g>
+    </g>
+    <rect id="Rectangle 4" x="0.5" y="213.5" width="69" height="69" rx="9.5" transform="rotate(-90 0.5 213.5)" stroke="url(#paint1_linear_370_23)"/>
+    <rect id="Rectangle 5" x="250.5" y="213.5" width="69" height="69" rx="9.5" transform="rotate(180 250.5 213.5)" stroke="url(#paint2_linear_370_23)"/>
+    <rect id="Rectangle 3" x="250.5" y="0.5" width="69" height="69" rx="9.5" transform="rotate(90 250.5 0.5)" stroke="url(#paint3_linear_370_23)"/>
+    <rect id="Rectangle 2" x="0.5" y="0.5" width="69" height="69" rx="9.5" stroke="url(#paint4_linear_370_23)"/>
+    </g>
+    </g>
+    <defs>
+    <filter id="filter0_d_370_23" x="81.2319" y="68.2562" width="70.5116" height="70.6315" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+    <feOffset dy="4"/>
+    <feGaussianBlur stdDeviation="2"/>
+    <feComposite in2="hardAlpha" operator="out"/>
+    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_370_23"/>
+    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_370_23" result="shape"/>
+    </filter>
+    <linearGradient id="paint0_linear_370_23" x1="122" y1="100" x2="95" y2="123" gradientUnits="userSpaceOnUse">
+    <stop stop-color="white"/>
+    <stop offset="1" stop-opacity="0"/>
+    </linearGradient>
+    <linearGradient id="paint1_linear_370_23" x1="21" y1="228" x2="25" y2="232" gradientUnits="userSpaceOnUse">
+    <stop stop-color="white"/>
+    <stop offset="1"/>
+    </linearGradient>
+    <linearGradient id="paint2_linear_370_23" x1="272" y1="228" x2="276" y2="232" gradientUnits="userSpaceOnUse">
+    <stop stop-color="white"/>
+    <stop offset="1"/>
+    </linearGradient>
+    <linearGradient id="paint3_linear_370_23" x1="272" y1="14" x2="276" y2="18" gradientUnits="userSpaceOnUse">
+    <stop stop-color="white"/>
+    <stop offset="1"/>
+    </linearGradient>
+    <linearGradient id="paint4_linear_370_23" x1="21" y1="14" x2="25" y2="18" gradientUnits="userSpaceOnUse">
+    <stop stop-color="white"/>
+    <stop offset="1"/>
+    </linearGradient>
+    <clipPath id="clip0_370_23">
+    <rect width="75" height="75" fill="white" transform="matrix(0 1 -1 0 154 62)"/>
+    </clipPath>
+    </defs>
+    <text className={styles.computerRepairText} x="15" y="25" text-anchor="start" fill="white" font-size="18" font-weight="bold" font-family="Arial, sans-serif">
+        Computer Repairs
+    </text>
+</svg>
+
+)
+
+}
