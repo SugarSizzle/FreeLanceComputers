fe381c88e87ec00dbbdd687e14a4dba01d961ba6
SugarSizzle <george.leon41@yahoo.com>
2025-10-28 01:57:14 -0400

    just some progress for admin page

---
+++ src/Pages/AdminDashboard/AdminOverViewPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AdminOverviewHeroSection } from '../../components/AdminOverview/AdminOverviewHeroSection'
+
+export const AdminOverViewPage = () => {
+    return (
+        <div>
+            <AdminOverviewHeroSection />
+        </div>
+    )
+}
