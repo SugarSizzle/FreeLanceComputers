fd39b40225ac2031fdb0992129a7fe29bcb89519
SugarSizzle <george.leon41@yahoo.com>
2025-11-04 15:10:40 -0500

    just small tweaks
    small tweaks to UI design

--- src/Pages/AdminDashboard/AdminOverViewPage.jsx
+++ src/Pages/AdminDashboard/AdminOverViewPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { AdminOverviewHeroSection } from '../../components/AdminOverview/AdminOverviewHeroSection'
 import { AdminServicesGrid } from '../../components/AdminOverview/AdminServicesGrid'
+import { DashboardNav } from '../../Layout/DashboardNav'
+
 export const AdminOverViewPage = () => {
     return (
         <div>
+            <DashboardNav  />
             <AdminOverviewHeroSection />
             <AdminServicesGrid />
         </div>
