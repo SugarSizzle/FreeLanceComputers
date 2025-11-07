2afab8859f997cd014955ea01051962cdc4f9697
SugarSizzle <george.leon41@yahoo.com>
2025-11-07 15:05:38 -0500

    admin timeline progress
    Did some learning of Express along with working on the admin timeline feature

--- server/index.js
+++ server/index.js
@@ -8,19 +8,18 @@ const app = express();
 app.use(cors());
 
 
+
+
 app.get("/products", async (req, res) => {
   const { data, error } = await supabase.from("products").select("*");
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+
 
 export const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.VITE_SUPABASE_URL,
+    process.env.VITE_SUPABASE_ANON_KEY
   );
   
