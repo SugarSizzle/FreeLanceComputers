5c85ff5a1dcba2d181293c66c43a0e147d3fa285
SugarSizzle <george.leon41@yahoo.com>
2025-11-10 15:07:44 -0500

    Database practice
    Learning how to make and insert data in a database

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { servicesController } from '../controllers/servicesControll';
-import { productsController } from '../controllers/productsController';
+import { servicesController } from '../controllers/servicesControll.js';
+import { productsController } from '../controllers/productsController.js';
 
 const apiRouter = express.Router();
 
 apiRouter.get('/services', servicesController);
 apiRouter.get('/products', productsController);
+
+export default apiRouter;
