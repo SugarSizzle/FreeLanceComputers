9219d4bfdc2d0f9adc7dea27c28bc9644a515abb
SugarSizzle <george.leon41@yahoo.com>
2025-12-29 14:59:39 -0500

    finally back after pc issues
    I am finally back

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { CartMainSection } from '../components/CartComponents/CartMainSection'
+import { useCart } from '../Context/CartContext'
 
 
 export const CartPage = () => {
+    const { clearCartNotification } = useCart();
+
+    useEffect(() => {
+        clearCartNotification();
+    }, [clearCartNotification]);
+
     return (
         <>
         <CartMainSection />
