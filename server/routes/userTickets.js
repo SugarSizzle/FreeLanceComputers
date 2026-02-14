e5f9de38e2ed48b58692350a72bec01e3a42bfda
SugarSizzle <george.leon41@yahoo.com>
2026-02-14 17:32:15 -0500

    Most Recent Functionality
    Made it where the most recent tab in the dashboard works for users. Shows the most recent change made to their services.

--- server/routes/userTickets.js
+++ server/routes/userTickets.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { getUserTickets } from '../controllers/userTicketsController.js'
+import { getUserTickets, getLatestServiceUpdate } from '../controllers/userTicketsController.js'
 import { getUserTicketTimeline } from '../controllers/userTimelineController.js'
 
 const userTicketsRouter = express.Router()
 
 userTicketsRouter.get('/', getUserTickets)
 userTicketsRouter.get('/timeline/:id', getUserTicketTimeline)
+userTicketsRouter.get('/latest-update', getLatestServiceUpdate)
 
 export default userTicketsRouter
 
